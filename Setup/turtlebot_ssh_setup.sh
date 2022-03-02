bfd5e448abd985fe522fe94990f23a0ad8efd922
Alexander <alexander.mattes99@gmx.de>
2022-03-02 11:20:32 +0100

    backup

---
+++ Setup/turtlebot_ssh_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Geben sie die IP des remote PC ein:" IP_OF_REMOTE_PC;
+ROS_MASTER_URI="http://${IP_OF_REMOTE_PC}$:11311"
+
+
+
+
+#!/bin/bash
+
+read -p "Geben sie die IP des Turtlebot ein:" IP_OF_TURTLEBOT;
+ROS_HOSTNAME="$IP_OF_TURTLE_BOT"
+
